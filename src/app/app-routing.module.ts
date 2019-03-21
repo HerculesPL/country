adf7396053e40d93e21800328a797f5a8b7b7f9b
Hércules Pedrosa Lemos Junior <hercules.lemos@lais.huol.ufrn.br>
2019-03-21 02:09:02 -0300

    ajust route in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CountryListComponent } from './countries/country-list/country-list.component';
+import { CountryListResolver } from './countries/country-list/country-list-resolver';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
+
+const routes: Routes = [         
+  { 
+      path: '', 
+      component: CountryListComponent,
+      resolve: {
+          countries: CountryListResolver
+      }
+  },
+    { 
+        path: '**', 
+        component: NotFoundComponent
+    }  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
